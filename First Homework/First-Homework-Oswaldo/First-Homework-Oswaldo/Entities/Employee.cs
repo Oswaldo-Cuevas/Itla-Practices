c695f051666dcf5bdd1bcaee102f49c17dce3aa0
Oswaldo-Cuevas <oswaldocuevasfeliz2014@gmail.com>
2026-02-14 18:20:44 -0800

    Fix nested repo and add homeworks

---
+++ First Homework/First-Homework-Oswaldo/First-Homework-Oswaldo/Entities/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace First_Homework_Oswaldo.Entities
+{
+    public abstract class Employee : CommunityMember
+    {
+        public string Cargo { get; set; }
+        public decimal SalarioBase { get; set; }
+        public DateTime FechaIngreso { get; set; }
+
+        public virtual decimal CalcularSalario()
+        {
+            return SalarioBase;
+        }
+    }
+}
---
+++ First Homework/First-Homework-Oswaldo/First-Homework-Oswaldo/Entities/Professor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace First_Homework_Oswaldo.Entities
+{
+    public class Professor : Teacher
+    {
+        public string NivelEducativo { get; set; }
+
+        public void ImpartirClase()
+        {
+            Console.WriteLine($"Impartiendo la asignatura {Asignatura}");
+        }
+    }
+}
